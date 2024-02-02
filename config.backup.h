53c33910b5866038a1ee6f02d799dd02f55b429b
Shubham <132815243+versus2004@users.noreply.github.com>
2024-02-02 23:31:32 +0530

    Update config.backup.h

--- config.backup.h
+++ config.backup.h
@@ -19,9 +19,11 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7"};
 
 static const Rule rules[] = {
+
+
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
@@ -96,6 +98,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	//volume controls 
+
 	{0,0x1008ff11,spawn,SHCMD("amixer -q sset Master 5%-")},
 	{0,0x1008ff13,spawn,SHCMD("amixer -q sset Master 5%+")},
 	
